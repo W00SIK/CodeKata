250babc9377a8e7ab6d9cbefc244f33c4d052a9e
unknown <rhrod123456@gmail.com>
2024-01-03 09:37:36 +0900

    53

--- 53.js
+++ 53.js
@@ -1,11 +1,17 @@
 function solution(k, score) {
     var answer = [];
     let m = []
-    for (let i = 0; i<score.length; i++) {
-            m.push(score[i])
+    let n = []
+    for (let i = 0; i < score.length; i++) {
+        m.push(score[i])
+        if (m.length < k) {
+            m.sort((a, b) => a - b)
+            answer.push(m[0])
+        } else if (m.length >= k) {
             m.sort((a, b) => b - a)
+            answer.push(m[k-1])
         }
-    // console.log(answer)
+    }
     return answer;
 }
 
